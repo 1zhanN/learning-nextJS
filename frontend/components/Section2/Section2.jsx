65adef4276b6620c34dc5d23bb5f9b309581b2c6
ehtisham <izhann000@gmail.com>
2024-05-04 18:50:57 +0500

    responsive
    changed structure, used grids, removed extra paddings and margins

--- frontend/components/Section2/Section2.jsx
+++ frontend/components/Section2/Section2.jsx
@@ -10,20 +10,20 @@ const Section2 = () => {
         Empowering growth and success for all stakeholders through our unique approach to delivering exceptional experiences. Join us as we ascend to new heights through the transformative power of digital innovation.
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:gap-10 text-center">
-          <div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-2 sm:gap-10 text-center">
+          <div className="col-span-1">
             <h3 className="text-4xl font-bold mb-2">100+</h3>
             <p>Served Clients Across the Globe</p>
           </div>
-          <div>
+          <div className="col-span-1">
             <h3 className="text-4xl font-bold mb-2">10+</h3>
             <p>Team Members and Advisors</p>
           </div>
-          <div >
+          <div  className="col-span-1">
             <h3 className="text-4xl font-bold mb-2">10+</h3>
             <p>Projects Built and Deployed</p>
           </div>
-          <div>
+          <div className="col-span-1">
             <h3 className="text-4xl font-bold mb-2">4+</h3>
             <p>Years of Excellence</p>
           </div>
--- frontend/components/Section3/Section3.jsx
+++ frontend/components/Section3/Section3.jsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
 const Section3 = () => {
   return (
-    <div className="my-20">
+    <div className="my-4">
       <div className="flex justify-center mx-20 lg:mx-80">
         <p className=" md:text-2xl font-bold text-gray-800 my-10">
            We earn <span className="text-green-700">Trust</span> by committing to provide value and consistently delivering on that promise
         </p>
       </div>
-      <div className="flex justify-center gap-20">
+      <div className="flex justify-center mt-4 gap-20">
         <div>
           <div className="relative w-[50px] h-[50px] md:w-[100px] md:h-[100px]">  
-            <Image src="/images/ikea.png" alt="Logo" fill />
+            <Image 
+            src="/images/ikea.png" 
+            alt="Logo" 
+            fill 
+            className="object-fit"/>
           </div>
-          <div className="relative w-[50px] h-[50px] md:w-[100px] md:h-[100px] ">
-            <Image src="/images/uber.png" alt="Logo" fill />
+          <div className="relative mt-4 w-[50px] h-[50px] md:w-[100px] md:h-[100px] ">
+            <Image 
+            src="/images/uber.png" 
+            alt="Logo" 
+            fill 
+            className="object-fit"/>
           </div>
         </div>
         <div>
@@ -25,23 +33,29 @@ const Section3 = () => {
               className="object-fit"
             />
           </div>
-          <div className="relative w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
+          <div className="relative mt-4 w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
             <Image
               src="/images/steam.png"
               alt="Logo"
               fill
+              className="object-fit"
             />
           </div>
         </div>
         <div>
           <div className="relative w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
-            <Image src="/images/ms.png" alt="Logo" fill />
+            <Image 
+            src="/images/ms.png" 
+            alt="Logo" 
+            fill 
+            className="object-fit"/>
           </div>
-          <div className="relative w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
+          <div className="relative mt-4 w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
             <Image
               src="/images/mazda.png"
               alt="Logo"
               fill
+              className="object-fit"
             />
           </div>
         </div>
